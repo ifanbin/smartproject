c131887ac923332cf4778fe366126cfcea1b55bb
ifanbin <ifanbin@outlook.com>
2019-11-23 16:00:45 +0800

    增加web异常处理

--- src/main/java/com/hzlgit/project/HomeController.java
+++ src/main/java/com/hzlgit/project/HomeController.java
@@ -1,8 +1,13 @@
 package com.hzlgit.project;
 
+import cn.hutool.core.exceptions.StatefulException;
+import com.hzlgit.project.customer.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +18,14 @@ public class HomeController {
     public String home(){
         log.info("access home! \noutput: hello world!");
         return "hello world";
+    }
+
+    @RequestMapping("/testException/{id}")
+    public Long getCustomer(@PathVariable Long id){
+        log.info("异常测试 id == " + id );
+        if(-1 == id)
+            throw new StatefulException(HttpStatus.BAD_REQUEST.value(), "后台有异常！");
+        else
+            return id;
     }
 }
